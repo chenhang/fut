8522e4d9a1f36245a2ff61f91c307e363ace9171
hang <hang22c@gmail.com>
2016-12-10 01:53:53 +0800

    ADD cheapest squad

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -5,4 +5,8 @@ class Challenge < ActiveRecord::Base
   has_many :squads
   validates_presence_of :url
   validates_uniqueness_of :url
+
+  def cheapest_squad
+    squads.sort_by {|s| s.total_prize}.first
+  end
 end
